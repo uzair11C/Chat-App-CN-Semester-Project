1187a786d2dbfa48308ac297bb21bacb3145423d
Uzair Abdullah <uzairabdullah973@gmail.com>
2022-06-03 18:07:39 +0500

    created repository for group

---
+++ client/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useRef ,useState} from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidV4 } from 'uuid'
+
+export default function Login({ onIdSubmit }) {
+  
+  const [newID,setNewID] = useState('')
+  
+  const idRef = useRef()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    onIdSubmit(idRef.current.value)
+  }
+
+  const getNewID = event =>
+  {
+    setNewID(event.target.value)
+  }
+
+  function createNewId() {
+    onIdSubmit(newID)
+  }
+
+  return (
+    <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
+      <Form onSubmit={handleSubmit} className="w-100">
+        <Form.Group>
+          <Form.Label>Enter Your ID:</Form.Label>
+          <Form.Control type="text" ref={idRef} required />
+        </Form.Group>   
+        <Button type="submit" className="mr-2">Login</Button>
+        <h2 style={{marginTop: "20px"}} >Not a User?</h2>
+        <h3>Create a new ID</h3>
+        <Form.Group>
+          <Form.Label>Type your number to create ID:</Form.Label>
+          <Form.Control type="text" ref={idRef} required onchange={getNewID}  />
+        </Form.Group> 
+        <Button onClick={createNewId} variant="secondary">Create A New Id</Button>
+      </Form>
+    </Container>
+  )
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const port = 5000 || '0.0.0.0'
+const io = require('socket.io')(port)
+
+io.on('connection', socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
+  console.log("Listening at " + port)
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
+  })
+})
